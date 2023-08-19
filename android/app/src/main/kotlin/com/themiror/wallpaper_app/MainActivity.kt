90bd86fd7a8fc49ed8310df7a1b8cd55a5e6d658
partha <partarc94@gmail.com>
2023-08-19 15:01:51 +0600

    appbar

---
+++ android/app/src/main/kotlin/com/themiror/wallpaper_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.themiror.wallpaper_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/widgets/CustomAppBar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RichText(
+        text: TextSpan(children: [
+          TextSpan(text: "habijabi")
+        ]),
+      ),
+    );
+  }
+}
